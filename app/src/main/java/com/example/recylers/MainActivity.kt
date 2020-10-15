eb736d8f694b97e3db39b9fc164a7128dc9dd9de
fazil.s@lantrasoft.com <SinCos123>
2020-10-15 13:14:34 +0530

    ToDoLists

--- app/src/main/java/com/example/recylers/MainActivity.kt
+++ app/src/main/java/com/example/recylers/MainActivity.kt
@@ -1,4 +1,17 @@
 package com.example.recylers
+/// Todo 1: Create two rows with Image view
+/// Todo 2: Retrofit with recycler with single row
+/// Todo 3: Room with recycler with single row
+/// Todo 4: Create views   dynamically with single row with floating  action  bar click
+/// Todo 5: Swippable lists view
+/// Todo 6 : item click listner
+/// Todo 7 : Recycler with live data
+/// Todo 8 : Recycler with dataBinding
+/// Todo 9e : Expandable Listview
+/// Todo 10: View model with Recycler
+/// Todo 11: View Binding with Recycler
+/// Todo 12: MVVM with view model,Data binding,Live data,Room and retrofit and recycler
+
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
